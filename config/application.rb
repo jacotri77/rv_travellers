3609b8ca328ab1ea151eb51f4bf3d7eb78d97394
Erik Jacobsen <erikjacobsen@X-100022-LV.local>
2020-11-02 08:41:58 -0800

    rolling back sprockets version to combat segfault issue when pushing to Heroku

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,9 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
+Rails.application.config.assets.configure do |env|
+  env.export_concurrent = false
+end
 module RvTravellers
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
