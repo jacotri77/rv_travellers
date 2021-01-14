4f749209c1a6a10ade9e0ecd6f0de723426ce631
Erik Jacobsen <erikjacobsen@X-100022-LV.local>
2021-01-14 14:56:44 -0800

    fixing the gem still

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,7 +15,8 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-require("trix")
-require("@rails/actiontext")
+
 
 import "actiontext"
+require("trix")
+require("@rails/actiontext")
